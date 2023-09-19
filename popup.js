38fb4987d82e71c836af9f7cb86929f89fa5ae8e
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-09-19 16:14:38 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,17 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const fetchButton = document.getElementById("fetchButton");
-  const resultDiv = document.getElementById("result");
-
-  fetchButton.addEventListener("click", () => {
-    // Send a message to the background script to fetch JSON
-    chrome.runtime.sendMessage({ action: "fetchJSON" });
-  });
-
-  // Listen for a response message from the background script
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "jsonResponse") {
-      // Display the JSON data in the popup
-      resultDiv.textContent = JSON.stringify(message.data, null, 2);
-    }
+document.getElementById('startButton').addEventListener('click', () => {
+  chrome.scripting.executeScript({
+    target: { tabId: chrome.tabs.getCurrent().id },
+    function: startMonitoring
   });
 });
+
+function startMonitoring() {
+  chrome.runtime.sendMessage({ action: 'startMonitoring' });
+}
