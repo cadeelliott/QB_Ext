bceb565d85e78c3a23ab38d2bdff5f2113f753fc
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-02 16:46:57 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,18 +1,19 @@
-let monitoring = false;
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'startMonitoring') {
-    if (!monitoring) {
-      chrome.webRequest.onCompleted.addListener(
-        logResponse,
-        { urls: ['<all_urls>'] }
-      );
-      monitoring = true;
-    }
+chrome.debugger.onDetach.addListener(function (source, reason) {
+  if (reason === 'target_closed') {
+    // Handle the tab being closed
   }
 });
 
-function logResponse(details) {
-  console.log(details.url);
-  console.log(details.responseHeaders);
-}
+chrome.runtime.onConnect.addListener(function (port) {
+  port.onMessage.addListener(function (msg) {
+    if (msg.action === 'attach') {
+      chrome.debugger.attach({ tabId: msg.tabId }, '1.2', function () {
+        chrome.debugger.sendCommand({ tabId: msg.tabId }, 'Network.enable', {}, function () {
+          // You can now use Network.getResponseBody to get response bodies
+        });
+      });
+    } else if (msg.action === 'detach') {
+      chrome.debugger.detach({ tabId: msg.tabId });
+    }
+  });
+});
