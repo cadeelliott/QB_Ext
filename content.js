f6fe944c49608e22e60f1aca32e5955a6717aba5
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-02 16:47:15 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,17 +1,1 @@
-// content.js
-
-// This function extracts and logs text content from the page
-function logTextContent() {
-  const textContent = document.body.innerText;
-  console.log('Text content from the current page:');
-  console.log(textContent);
-}
-
-// Add an event listener to listen for messages from the extension popup or background script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'extractTextContent') {
-    logTextContent();
-  }
-});
-
-// You can add more code here to interact with the web page's content as needed.
+chrome.runtime.connect({ name: 'network-extension' }).postMessage({ action: 'attach', tabId: chrome.devtools.inspectedWindow.tabId });
