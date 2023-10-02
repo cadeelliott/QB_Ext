5c2696b76df80a2c26445efa466e7713022f772c
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-02 16:50:35 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-chrome.runtime.connect({ name: 'network-extension' }).postMessage({ action: 'attach', tabId: chrome.devtools.inspectedWindow.tabId });
+// content.js
+
+// Function to send a message to the background script
+function sendMessageToBackground(action, data) {
+  chrome.runtime.connect({ name: 'network-extension' }).postMessage({ action, ...data });
+}
+
+// Example: Send a message to the background script to retrieve response body for a specific network request
+// Replace 'yourRequestIdHere' with the actual requestId
+const requestIdToRetrieve = 'yourRequestIdHere';
+
+sendMessageToBackground('getResponseBody', {
+  tabId: chrome.devtools.inspectedWindow.tabId,
+  requestId: requestIdToRetrieve,
+});
+
+// You can add more code here to interact with the web page as needed
