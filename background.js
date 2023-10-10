fe930233a98dac87e4d75fa9f6d8c64b1d8d40a2
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-10 14:38:13 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,39 +1,25 @@
 // background.js
-
-// Function to handle network request
-function handleNetworkRequest(tabId, requestId) {
-  chrome.debugger.sendCommand({ tabId: tabId }, 'Network.getResponseBody', { requestId: requestId }, function (result) {
-    if (chrome.runtime.lastError) {
-      // Handle errors
-      console.error(chrome.runtime.lastError);
-      return;
-    }
-
-    const responseBody = result.body;
-    // Use the response body as needed
-    console.log(responseBody);
-  });
-}
-
-chrome.debugger.onDetach.addListener(function (source, reason) {
-  if (reason === 'target_closed') {
-    // Handle the tab being closed
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "duplicateElements") {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const activeTab = tabs[0];
+      if (activeTab) {
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          function: duplicateElements,
+        });
+      } else {
+        console.error("No active tab found.");
+      }
+    });
   }
 });
 
-chrome.runtime.onConnect.addListener(function (port) {
-  port.onMessage.addListener(function (msg) {
-    if (msg.action === 'attach') {
-      chrome.debugger.attach({ tabId: msg.tabId }, '1.2', function () {
-        chrome.debugger.sendCommand({ tabId: msg.tabId }, 'Network.enable', {}, function () {
-          // You can now use Network.getResponseBody to get response bodies
-        });
-      });
-    } else if (msg.action === 'detach') {
-      chrome.debugger.detach({ tabId: msg.tabId });
-    } else if (msg.action === 'getResponseBody') {
-      // Call the function to retrieve the response body
-      handleNetworkRequest(msg.tabId, msg.requestId);
-    }
+function duplicateElements() {
+  const elementsToDuplicate = document.querySelectorAll(".cell.col-1.left.wrap");
+
+  elementsToDuplicate.forEach((element) => {
+    const clone = element.cloneNode(true);
+    element.parentNode.insertBefore(clone, element.nextSibling);
   });
-});
+}
