63f69d211e510dc0f6bece8a6a375b5f89bf51fa
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-10 14:05:44 -0500

    Update popup.js
    Error handling

--- popup.js
+++ popup.js
@@ -3,12 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const duplicateButton = document.getElementById("duplicateButton");
 
   duplicateButton.addEventListener("click", function () {
-    // Send a message to content.js to trigger the duplication
+    // Get the currently active tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id },
-        function: duplicateElements,
-      });
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+
+      const activeTab = tabs[0];
+
+      // Check if an active tab was found
+      if (activeTab) {
+        // Send a message to content.js to trigger the duplication
+        chrome.scripting.executeScript({
+          target: { tabId: activeTab.id },
+          function: duplicateElements,
+        });
+      } else {
+        console.error("No active tab found.");
+      }
     });
   });
 
