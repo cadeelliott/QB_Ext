6e1cf3d93cd5785c6834197f9aa89d04eb2a7e38
cadeelliott <137833718+cadeelliott@users.noreply.github.com>
2023-10-10 14:40:49 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,34 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const duplicateButton = document.getElementById("duplicateButton");
 
   duplicateButton.addEventListener("click", function () {
-    // Get the currently active tab
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (chrome.runtime.lastError) {
-        console.error(chrome.runtime.lastError);
-        return;
-      }
-
-      const activeTab = tabs[0];
-
-      // Check if an active tab was found
-      if (activeTab) {
-        // Send a message to content.js to trigger the duplication
-        chrome.scripting.executeScript({
-          target: { tabId: activeTab.id },
-          function: duplicateElements,
-        });
-      } else {
-        console.error("No active tab found.");
-      }
-    });
+    // Send a message to the background script to trigger the duplication
+    chrome.runtime.sendMessage({ action: "duplicateElements" });
   });
-
-  function duplicateElements() {
-    const elementsToDuplicate = document.querySelectorAll(".cell.col-1.left.wrap");
-
-    elementsToDuplicate.forEach((element) => {
-      const clone = element.cloneNode(true);
-      element.parentNode.insertBefore(clone, element.nextSibling);
-    });
-  }
 });
